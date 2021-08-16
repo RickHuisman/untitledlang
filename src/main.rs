9145f6add7a7992f038f7debc567148304240b25
rickhuis <rick.huisman@live.nl>
2021-08-16 11:18:51 +0200

    Add repl

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,42 @@
 use crate::lexer::lexer::lex;
 use crate::parser::parser::parse;
+use std::io;
+use std::borrow::BorrowMut;
 
 mod lexer;
 mod parser;
 
 fn main() {
-    let mut tokens = lex("let x = 10;").unwrap();
-    let ast = parse(&mut tokens);
-    println!("foobar");
+    run_repl();
+}
+
+pub fn run_repl() {
+    loop {
+        let line = read_line();
+        let mut tokens = lex(&line).unwrap();
+        let ast = parse(&mut tokens).unwrap();
+        println!("{:?}", ast);
+    }
+}
+
+fn read_line() -> String {
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Ok(n) => {
+            trim_newline(input.borrow_mut());
+            input
+        }
+        Err(error) => {
+            panic!("error: {}", error);
+        }
+    }
+}
+
+fn trim_newline(s: &mut String) {
+    if s.ends_with('\n') {
+        s.pop();
+        if s.ends_with('\r') {
+            s.pop();
+        }
+    }
 }
